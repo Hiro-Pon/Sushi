284013003ed3a3edf13894fb0504899a97c07da5
Hiroya NAKAJIMA <hiroya.nakajima@gmail.com>
2018-07-05 10:53:08 +0900

    Initial Commit

---
+++ Sushi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sushi
+//
+//  Created by 中嶋裕也 on 2018/07/05.
+//  Copyright © 2018年 中嶋裕也. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sushi/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sushi
+//
+//  Created by 中嶋裕也 on 2018/07/05.
+//  Copyright © 2018年 中嶋裕也. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
